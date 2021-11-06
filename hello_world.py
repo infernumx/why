6f03a708f47e51a76992f3446f0dcde047c4b0d2
infernumx <infernumx@protonmail.com>
2021-11-06 04:50:43 -0700

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+_=print;__=type;___=setattr;____=getattr;______=len;o_o=__("",(),{"__init__":lambda _,__:___(_,"_",__),"__repr__":lambda _:____(_,"_",_),"__add__":lambda _,__: o_o(____(_,"_")+____(__,"_")),"__sub__":lambda _,__: o_o(____(_,"_")[:______(____(__,"_"))]),})
+h=e=l=l=o=w=o=r=l=d=o__o=o_o("hello world");_(h+e-l+l-o+w-o+r-l+d-o__o);
